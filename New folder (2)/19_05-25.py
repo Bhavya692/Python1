cb8762cb35fcc43a678842aa3f4f600f43449383
Bhavya692 <159572108+Bhavya692@users.noreply.github.com>
2025-09-27 22:27:45 +0530

    Delete New folder (2) directory

--- New folder (2)/19_05-25.py
+++
@@ -1,8 +0,0 @@
-M=[[0,2,4],[1,2,7],[5,3,2]]
-res=[]
-for ind1 in range(len(M)-1,-1,-1):
-    sublist=[]
-    for ind2 in range(len(M)):
-        sublist.append(M[ind2][ind1])
-    res.append(sublist)
-print(res)
